a37e74fee42dc02202c3046261679f776adea33b
hoki8823 <hoki8823@github.com>
2021-11-10 11:41:27 +0900

    프로퍼티와 초기화

---
+++ src/chapter06/section3/ObjectExpressionSuperMan.kt
@@ -0,0 +1,16 @@
+package chapter06.section3
+
+open class Superman() {
+    fun work() = println("Taking photos")
+    fun talk() = println("Taking with people")
+    open fun fly() = println("Flying in the air")
+}
+
+fun main() {
+    val pretendedMan = object: Superman() {
+        override fun fly() = println("I'm not a real superman. I can't fly!")
+    }
+    pretendedMan.work()
+    pretendedMan.talk()
+    pretendedMan.fly()
+}
