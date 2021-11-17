e86da83d599aff165464da6d095ee91bc1a75d6f
hoki8823 <hoki8823@naver.com>
2021-11-17 15:35:58 +0900

    다양한 클래스와 인터페이스

---
+++ src/chapter07/section1/AbstractObject.kt
@@ -0,0 +1,15 @@
+package chapter07.section1
+
+abstract class Printer {
+    abstract fun print() // 추상 메서드
+}
+
+val myPrinter = object: Printer() { // 객체 인스턴스
+    override fun print() {
+        println("출력합니다.")
+    }
+}
+
+fun main() {
+    myPrinter.print()
+}
